4df586713ce0c271f937b6f4ec61afc8850d688e
asimenstad <a-nna-s@hotmail.com>
2022-03-18 17:38:45 +0100

    Added cart counter

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,25 +1,52 @@
 /// Add the products to cart
-const cartButton = document.querySelectorAll(".add-to-cart-button");
+const cartButton = document.querySelector(".add-to-cart-button");
+const cartCounter = document.querySelector(".cart-counter p");
 let cartArray = [];
-let total = 0;
 
-cartButton.forEach(function (button) {
-  button.onClick = function (event) {
-    cartArray.push(product);
-    localStorage.setItem("cartList", JSON.stringify(cartArray));
-  };
-});
+cartButton.addEventListener("click", addAmount);
 
-const cartItems = JSON.parse(localStorage.getItem("cartList"));
-const cartContainer = document.querySelector(".checkout-items__container");
+function loadCartCounter() {
+  let productCounter = localStorage.getItem("cartNumber");
 
-function addToCart() {
-  cartContainer.innerHTML = "";
-
-  total += product.price;
-  cartItems.forEach(function () {
-    cartContainer.innerHTML += ` <div>
-          <h3>${product.name}</h3>
-        </div>`;
-  });
+  if (productCounter) {
+    cartCounter.textContent = productCounter;
+  }
 }
+
+function addAmount() {
+  let productCounter = localStorage.getItem("cartNumber");
+
+  productCounter = parseInt(productCounter);
+
+  if (productCounter) {
+    localStorage.setItem("cartNumber", productCounter + 1);
+    cartCounter.textContent = productCounter + 1;
+  } else {
+    localStorage.setItem("cartNumber", 1);
+    cartCounter.textContent = 1;
+  }
+
+  addItem();
+  totalPrice();
+}
+
+function addItem() {
+  cartArray.push(product);
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartArray));
+  cartArray = localStorage.getItem("productsInCart");
+  cartArray = JSON.parse(cartArray);
+}
+
+function totalPrice() {
+  let total = localStorage.getItem("totalPrice");
+
+  if (total !== null) {
+    total = parseInt(total);
+    localStorage.setItem("totalPrice", total + product.price);
+  } else {
+    localStorage.setItem("totalPrice", product.price);
+  }
+}
+
+loadCartCounter();
