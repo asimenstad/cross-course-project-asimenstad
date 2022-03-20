19219557fb0dec538e4545a4577bad2cc84304f9
asimenstad <a-nna-s@hotmail.com>
2022-03-20 13:07:29 +0100

    Fix cart css

--- js/cart.js
+++ js/cart.js
@@ -7,12 +7,15 @@ function addProducts() {
   console.log(cartItems);
   for (let i = 0; i < cartItems.length; i++) {
     cartContainer.innerHTML += ` <div class="cart-item">
+    <div class="cart-item__product">
     <img src="${cartItems[i].image}" />
     <div class="cart-item__info">
 <h3>${cartItems[i].name}</h3>
 <p>${cartItems[i].price} KR</p>
 <p>Size: </p>
 </div>
+</div>
+<div class="remove-item"><i class="far fa-trash-alt remove"></i></div>
   </div>  
   <hr />`;
   }
