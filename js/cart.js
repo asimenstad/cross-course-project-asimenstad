059acfb6a3b9ecd6b8081de40538f73acf789c9f
asimenstad <a-nna-s@hotmail.com>
2022-03-22 12:18:33 +0100

    Update cart js and css

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,17 @@
 const cartTotalItems = document.querySelector(".total-items");
 const cartContainer = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
+const orderValue = document.querySelector(".order-value");
+const deliveryValue = document.querySelector(".delivery-value");
+const totalValue = document.querySelector(".total-value");
 
 function addProducts() {
   let cartItems = localStorage.getItem("productsInCart");
   cartItems = JSON.parse(cartItems);
 
   cartTotalItems.innerHTML = `<h2>(${cartItems.length})</h2>`;
+
+  cartContainer.innerHTML = "";
 
   for (let i = 0; i < cartItems.length; i++) {
     cartContainer.innerHTML += ` <div class="cart-item">
@@ -30,5 +35,6 @@ function addTotal() {
   totalSum = JSON.parse(totalSum);
 
   cartTotal.innerHTML = `<h3>Sum</h3><h3>${totalSum} KR</h3>`;
+  orderValue.textContent = totalSum;
 }
 addTotal();
